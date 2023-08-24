2affb7837a10cf7dc0bad096ee92f8b28657df1c
shamshaded <shamshaded@gmail.com>
2023-08-25 01:29:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,151 +1,164 @@
-import { fetchMovieAvailability, fetchMovieList } from "./api.js";
+import {fetchMovieAvailability,fetchMovieList} from "./api.js"
+const mainElement = document.getElementById('main');
+let bookerGridHolder = document.getElementById('booker-grid-holder');
+let bookTicketBtn = document.getElementById('book-ticket-btn');
+let booker = document.getElementById('booker');
 
-let mainContainerElement = document.querySelector(".main-container");
+async function renderMovies() {
+     mainElement.innerHTML = `<div id='loader'></div>`;
+     let movies = await fetchMovieList();
+     let movieHolder = document.createElement('div');
+     movieHolder.setAttribute('class', 'movie-holder');
+     movies.forEach(movie => {
+          createSeparateMovieTabs(movie, movieHolder);
+     });
+     mainElement.innerHTML = '';
+     mainElement.appendChild(movieHolder);
+     setEventToLinks();
+}
+renderMovies();
 
-let seatSelectorContainerElement = document.querySelector("#booker");
+function createSeparateMovieTabs(data, wrapper) {
+     let a = document.createElement('a');
+     a.setAttribute('data-movie-name', `${data.name}`);
+     a.classList.add('movie-link');
+     a.href = `#${data.name}`;
+     a.innerHTML = `<div class="movie" data-id=${data.name}>
+                    <div class="movie-img-wrapper" style="background-image:url(${data.imgUrl})"></div>
+                    <h4>${data.name}</h4>
+               </div>`;
+     wrapper.appendChild(a);
+}
 
-let seatSelectorHeaderElement = document.querySelector("#booker>h3");
 
-let seatSelectorButtonElement = document.querySelector("#book-ticket-btn");
+function setEventToLinks() {
+     let movieLinks = document.querySelectorAll('.movie-link');
+     movieLinks.forEach(movieLink => {
+          movieLink.addEventListener('click', (e) => {
+               renderSeatsGrid(movieLink.getAttribute('data-movie-name'));
+          })
+     })
+}
 
-let seatSelectorGridElement = document.querySelector("#booker-grid-holder");
-console.log(seatSelectorGridElement);
+async function renderSeatsGrid(movieName) {
+     bookerGridHolder.innerHTML = `<div id='loader'></div>`;
+     bookTicketBtn.classList.add('v-none');
+     let data = await fetchMovieAvailability(movieName);
+     renderSeats(data);
+     setEventsToSeats();
+}
 
-let createHtmlElementandAddContent = (elName, content) => {
-  let el = document.createElement(elName);
-  if (content) {
-    el.textContent = content;
-  }
-  return el;
-};
-let loader = createHtmlElementandAddContent("p", "loading...");
-loader.setAttribute("id", "loader");
+function renderSeats(data) {
+     if(booker.firstElementChild.tagName !== "H3"){
+          seatsSelected = [];
+          booker.innerHTML = `<h3 class="v-none">Seat Selector</h3>
+                              <div id="booker-grid-holder"></div>
+                              <button id="book-ticket-btn" class="v-none">Book my seats</button>`;
+     }
+     bookerGridHolder = document.getElementById('booker-grid-holder');
+     bookTicketBtn = document.getElementById('book-ticket-btn');
+     bookerGridHolder.innerHTML = '';
+     booker.firstElementChild.classList.remove('v-none');
+     createSeatsGrid(data)
+}
 
-let selectedSeatsArr = [];
+function createSeatsGrid(data){
+     let bookingGrid1 = document.createElement("div");
+     let bookingGrid2 = document.createElement("div");
+     bookingGrid2.classList.add("booking-grid");
+     bookingGrid1.classList.add("booking-grid");
+     for (let i = 1; i < 25; i++) {
+       let seat = document.createElement("div");
+       seat.innerHTML = i;
+       seat.setAttribute("id", `booking-grid-${i}`);
+       if (data.includes(i)) seat.classList.add("seat", "unavailable-seat");
+       else seat.classList.add("seat", "available-seat");
+       if (i > 12) bookingGrid2.appendChild(seat);
+       else bookingGrid1.appendChild(seat);
+     }
+     bookerGridHolder.appendChild(bookingGrid1);
+     bookerGridHolder.appendChild(bookingGrid2);
+     setTicketBooking();
+}
 
-let fetchSeats = (movieName) => {
-  seatSelectorContainerElement.appendChild(loader);
-  fetchMovieAvailability(movieName).then((seats) => {
-    loader.remove();
-    while (seatSelectorGridElement.lastChild) {
-      seatSelectorGridElement.removeChild(seatSelectorGridElement.lastChild);
-    }
-    createSeatsEl(seats);
-    createSeatsEl(seats, true);
-  });
-};
-let handleSelectedSeats = (e) => {
-  let el = e.target;
-  console.log(el);
-  if (el.classList.contains("available-seat")) {
-    el.classList.toggle("selected-seat");
-  }
-  if (el.classList.contains("selected-seat")) {
-    selectedSeatsArr.push(el.textContent);
-  } else {
-    selectedSeatsArr = selectedSeatsArr.filter((id) => id !== el.textContent);
-  }
-  if (selectedSeatsArr.length > 0) {
-    seatSelectorButtonElement.classList.remove("v-none");
-  } else {
-    seatSelectorButtonElement.classList.add("v-none");
-  }
-};
-let createSeatsEl = (availableSeats, nextSeats) => {
-  let gridWrapper = createHtmlElementandAddContent("div");
-  gridWrapper.classList.add("booking-grid");
-  let addValue = nextSeats ? 12 : 0;
+let seatsSelected = [];
+function setEventsToSeats() {
+     let AvaliableSeats = document.querySelectorAll('.available-seat');
+     AvaliableSeats.forEach(seat => {
+          seat.addEventListener('click', _ => {
+               saveSelectedSeat(seat);
+          })
+     })
+}
 
-  for (let i = 1 + addValue; i < 13 + addValue; i++) {
-    let seat = createHtmlElementandAddContent("td", i);
-    seat.setAttribute("id", `booking-grid-${i}`);
-    let className = availableSeats.includes(i)
-      ? "unavailable-seat"
-      : "available-seat";
-    seat.classList.add(className);
-    // if(seat.classList.contains("available-seat")){
-    seat.addEventListener("click", handleSelectedSeats);
-    // }
-    gridWrapper.append(seat);
-  }
-  seatSelectorGridElement.append(gridWrapper);
-};
+function saveSelectedSeat(seat) {
+     if (!seat.classList.contains("select-seat")) {
+          seat.classList.add('select-seat');
+          seatsSelected.push(seat.innerText);
+          bookTicketBtn.classList.remove('v-none');
+     } else {
+          seat.classList.remove('select-seat');
+          seatsSelected = seatsSelected.filter(item => seat.innerText !== item);
+          if (seatsSelected.length == 0){
+               bookTicketBtn.classList.add('v-none');
+          }
+     }
+}
+function setTicketBooking(){
+     bookTicketBtn.addEventListener('click', () => {
+          if (seatsSelected.length > 0) {
+               booker.innerHTML = '';
+               confirmTicket();
+          }
+     })
+}
 
-let createAMovie = (movieData) => {
-  let { name, imgUrl } = movieData;
-  let anc = document.createElement("a");
-  anc.setAttribute("class", "movie-link");
-  anc.setAttribute("href", `#${name}`);
-  let movieEl = `<div class="movie" data-d="${name}">
-    <div class="movie-img-wrapper" style="background-image: url('${imgUrl}'); background-size: cover;">
-    </div>
-    <h4>${name}</h4>
-    </div>`;
-  anc.innerHTML = movieEl;
-  anc.addEventListener("click", (e) => {
-    let { d } = e.target.parentElement.dataset;
-    seatSelectorHeaderElement.classList.remove("v-none");
-    fetchSeats(d);
-  });
-  return anc;
-};
+function confirmTicket(){
+     let confirmTicketElement = document.createElement('div');
+     confirmTicketElement.setAttribute('id', 'confirm-purchase');
+     let h3 = document.createElement('h3');
+     h3.innerText = `Confirm your booking for seat numbers:${seatsSelected.join(",")}`;
+     confirmTicketElement.appendChild(h3);
+     confirmTicketElement.appendChild(createForm());
+     booker.appendChild(confirmTicketElement);
+     success();
+}
 
-let addMovieList = () => {
-  mainContainerElement.appendChild(loader);
-  fetchMovieList().then((movieList) => {
-    loader.remove();
-    let div = document.createElement("div");
-    div.setAttribute("class", "movie-holder");
-    mainContainerElement.append(div);
-    movieList.forEach((movie) => {
-      let movieEl = createAMovie(movie);
-      div.append(movieEl);
-    });
-  });
-};
+function createForm(){
+     let form = document.createElement("form");
+     let formElements = `<input type="email" id="email" placeholder="email" required><br><br>
+                         <input type="tel" id="phone" placeholder="phone" required><br><br>
+                         <button id="submitBtn" type="submit">Purchase</button>`;
+     form.setAttribute("method", "post");
+     form.setAttribute("id", "customer-detail-form");
+     form.innerHTML = formElements;
+     return form;
+}
 
-let showFormToAddInformation = () => {
-  seatSelectorButtonElement.classList.add("v-none");
-  seatSelectorHeaderElement.classList.add("v-none");
-  let formEl = `
-    <section id="confirm-purchase">
-        <h2>Confirm your booking for seat numbers:7,8,9,13,14</h2>
-        <form id="customer-detail-form">
-            <label for="email">Email</label>
-            <input type="email" id="email" required/>
-            <br>
-            <br>
-            <label for="phone">phone No</label>
-            <input type="tel" id="phone" required/>
-            <br>
-            <br>
-            <button type="submit">purchase</button>
-        </form>
-    </section>
-    `;
-  seatSelectorGridElement.innerHTML = "";
-  seatSelectorGridElement.innerHTML = formEl;
+function success(){
+     let submitBtn = document.getElementById('submitBtn');
+     submitBtn.addEventListener('click',(e) => {
+         let form =  document.getElementById('customer-detail-form');
+         if(form.checkValidity()){
+              e.preventDefault();
+              let email = document.getElementById('email').value;
+              let phone = document.getElementById('phone').value;
+              renderSuccessMessage(email, phone);
+         }
+     })
+}
 
-  document
-    .querySelector("#customer-detail-form")
-    .addEventListener("submit", () => {
-      let email = document.querySelector("#email").value;
-      let phone = document.querySelector("#phone").value;
-      seatSelectorGridElement.innerHTML = "";
-      let successHtml = `
-        <section id="success">
-            <h2>Booking details</h2>
-            <div>seats:7,8,9,13,14</div>
-            <div>Phone No: ${phone}</div>
-            <div>Email: ${email}</div>
-        </section>
-        `;
-      seatSelectorGridElement.innerHTML = successHtml;
-    });
-};
+function renderSuccessMessage(email, phone){
+     booker.innerHTML = '';
+     createSuccessMessage(email, phone);
+}
 
-seatSelectorButtonElement.addEventListener("click", () => {
-  showFormToAddInformation();
-});
-
-addMovieList();
+function createSuccessMessage(email, phone){
+     let successElement = document.createElement("div");
+     successElement.setAttribute("id", "success");
+     successElement.innerHTML = `<h3>Booking details</h3>
+                               <p>Seats: ${seatsSelected.join(", ")}</p>
+                              <p>Email: ${email}</p>
+                              <p>Phone number: ${phone}</p>`;
+     booker.appendChild(successElement);
+}
